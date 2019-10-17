ea1fe8f4d25797a8d1a8b90ca7d9e6a713c9b821
Borgnia <borgniaf@amazon.com>
2019-10-17 15:50:31 +0100

    new app release

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-#terraform {
-#  backend "s3" {
-#    bucket = ""
-#    key    = "terraform_state_files/example_app.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "pipeline-dev-example-app-lhcbgnxz"
+    key    = "terraform_state_files/example_app.tfstate"
+    region = "us-east-1"
+  }
+}
 
 variable "name" {
   type = string
