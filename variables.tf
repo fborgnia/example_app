5eb135b5a79f7146ac2810e052ce0150165f6bb3
Borgnia <borgniaf@amazon.com>
2019-10-17 16:27:43 +0100

    new app release

--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "pipeline-dev-example-app-lhcbgnxz"
     key    = "terraform_state_files/example_app.tfstate"
     region = "us-east-1"
+    encrypt = true
+    kms_key_id = "80550670-1eed-492a-b42a-ae7104f0f448"
   }
 }
 
